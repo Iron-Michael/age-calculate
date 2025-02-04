9337d010404dcfdff60f2446b39a6bb5c7aef0b2
Sitthisak Logate <sitthisak@frevation.com>
2025-02-05 00:48:55 +0700

    adding-desktop-version-of-cal-app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,50 +1,72 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
 
 const Calculator = () => {
+  const [dayValue, setDayValue] = useState('');
+  const [monthValue, setMonthValue] = useState('');
+  const [yearValue, setYearValue] = useState('');
 
-  const [dayValue,setDayValue]=useState("")
-  const [monthValue, setMonthValue] = useState("")
-  const [yearValue, setYearValue] = useState("")
+  const [dayValueError, setDayValueError] = useState(false);
+  const [monthValueError, setMonthValueError] = useState(false);
+  const [yearValueError, setYearValueError] = useState(false);
 
-  const [dayValueError,setDayValueError]=useState(false)
-  const [monthValueError, setMonthValueError] = useState(false)
-  const [yearValueError, setYearValueError] = useState(false)
+  const [age, setAge] = useState({ years: '--', months: '--', days: '--' });
 
-  function setDay(value){
-    if (value < 32){
-      setDayValueError(false)
-      setDayValue(value)
-    }else{
-      setDayValueError(true)
-      setDayValue(value)
+  function setDay(value) {
+    if (value > 0 && value <= 31) {
+      setDayValueError(false);
+    } else {
+      setDayValueError(true);
     }
+    setDayValue(value);
   }
 
   function setMonth(value) {
-    if (value < 12) {
-      setMonthValueError(false)
-      setMonthValue(value)
+    if (value > 0 && value <= 12) {
+      setMonthValueError(false);
     } else {
-      setMonthValueError(true)
-      setMonthValue(value)
+      setMonthValueError(true);
     }
+    setMonthValue(value);
   }
 
   function setYear(value) {
-    if (value < 2025) {
-      setYearValueError(false)
-      setYearValue(value)
+    if (value > 1900 && value <= new Date().getFullYear()) {
+      setYearValueError(false);
     } else {
-      setYearValueError(true)
-      setYearValue(value)
+      setYearValueError(true);
     }
+    setYearValue(value);
   }
 
+  function calculateAge() {
+    if (dayValueError || monthValueError || yearValueError || !dayValue || !monthValue || !yearValue) {
+      alert('กรุณากรอกข้อมูลให้ถูกต้อง');
+      return;
+    }
+
+    const birthDate = new Date(yearValue, monthValue - 1, dayValue);
+    const today = new Date();
+
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+      months--;
+      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    setAge({ years, months, days });
+  }
 
   return (
     <section className='main-calculator'>
       <header className='label-header'>
-        <div className={dayValueError?'the-label-error':'the-label'} >
+        <div className={dayValueError ? 'the-label-error' : 'the-label'}>
           <label className='label-box'>DAY</label>
         </div>
         <div className={monthValueError ? 'the-label-error' : 'the-label'}>
@@ -54,27 +76,51 @@ const Calculator = () => {
           <label className='label-box'>YEAR</label>
         </div>
       </header>
-        <header className='main-header'>
-          <div className='the-box'>
-          <input className={dayValueError ? 'input-box-error' : 'input-box'} value={dayValue} onChange={(e) => setDay(e.target.value)}></input>
-          </div>
-          <div className='the-box'>
-          <input className={monthValueError ? 'input-box-error' : 'input-box'} value={monthValue} onChange={(e) => setMonth(e.target.value)}></input>
-          </div>
-          <div className='the-box'>
-          <input className={yearValueError ? 'input-box-error' : 'input-box'}  value={yearValue} onChange={(e) => setYear(e.target.value)} ></input>
-          </div>
-        </header>
-        <center style={{width:"100%",position:"relative"}}>
-        <div className='separator'>  
-            <div className='circle-navigator'>
-            <img src={'icon-arrow.svg'} className='img-arrow' ></img>
-            </div>
-        </div>
-      
-        </center>
-    </section>
-  )
-}
 
-export default Calculator
+      <header className='main-header'>
+        <div className='the-box'>
+          <input
+            className={dayValueError ? 'input-box-error' : 'input-box'}
+            value={dayValue}
+            onChange={(e) => setDay(e.target.value)}
+            type='number'
+          />
+        </div>
+        <div className='the-box'>
+          <input
+            className={monthValueError ? 'input-box-error' : 'input-box'}
+            value={monthValue}
+            onChange={(e) => setMonth(e.target.value)}
+            type='number'
+          />
+        </div>
+        <div className='the-box'>
+          <input
+            className={yearValueError ? 'input-box-error' : 'input-box'}
+            value={yearValue}
+            onChange={(e) => setYear(e.target.value)}
+            type='number'
+          />
+        </div>
+      </header>
+
+      <center style={{ width: '100%', position: 'relative' }}>
+        <div className='separator'>
+          
+            <button onClick={calculateAge} className='circle-navigator'>
+              <img src={'icon-arrow.svg'} className='img-arrow' alt='Calculate' />
+            </button>
+          
+        </div>
+      </center>
+
+      <div className='result-age-main'>
+        <div><label>{age.years} </label>years</div>
+        <div><label>{age.months}</label> months</div>
+        <div><label>{age.days}</label> days</div>
+      </div>
+    </section>
+  );
+};
+
+export default Calculator;
